579116517fe6da446954e012158d628853eeaeb6
KishanHegade <kishanhegade@gmail.com>
2024-08-24 10:46:26 +0530

    Initial commit

---
+++ src/main/java/com/jsp/edu/Edu/Tracking/System/EduTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.edu.Edu.Tracking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/edu/Edu/Tracking/System/EduTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.edu.Edu.Tracking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
