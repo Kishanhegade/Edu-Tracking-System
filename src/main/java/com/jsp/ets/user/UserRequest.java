f97688e54719f8ab267523f069c02c5764b05909
KishanHegade <kishanhegade@gmail.com>
2024-08-26 11:51:28 +0530

    password validation

--- src/main/java/com/jsp/ets/user/UserRequest.java
+++ src/main/java/com/jsp/ets/user/UserRequest.java
@@ -1,8 +1,10 @@
 package com.jsp.ets.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -12,7 +14,10 @@ public class UserRequest {
 	private String username;
 	 @Email(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$", message = "Email should be a valid Gmail address")
 	private String email;
-	
+	 @NotBlank(message = "Password is mandatory")
+	    @Size(min = 8, message = "Password must be at least 8 characters long")
+	    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$^&*._-]).{8,}$",
+	            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
 	private String password;
 	private Role role;
 	
