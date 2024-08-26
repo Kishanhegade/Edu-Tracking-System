a2f9768a0819084492a3824503e3d94841d6b1ca
KishanHegade <kishanhegade@gmail.com>
2024-08-26 11:47:58 +0530

    Email validation

--- src/main/java/com/jsp/ets/user/UserRequest.java
+++ src/main/java/com/jsp/ets/user/UserRequest.java
@@ -10,6 +10,7 @@ public class UserRequest {
 	@NotNull(message = "Username is mandatory")
 	@Pattern(regexp = "^[a-zA-Z0-9#$&*\\-_.!@]+$", message = "Invalid username format")
 	private String username;
+	 @Email(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$", message = "Email should be a valid Gmail address")
 	private String email;
 	
 	private String password;
