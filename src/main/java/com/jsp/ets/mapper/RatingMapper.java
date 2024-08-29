5c5da946f0e1324165ffeb9790cdfd43ee6014c2
KishanHegade <kishanhegade@gmail.com>
2024-08-29 11:32:10 +0530

    Updated Stack to have List of Subjects

--- src/main/java/com/jsp/ets/mapper/RatingMapper.java
+++ src/main/java/com/jsp/ets/mapper/RatingMapper.java
@@ -5,11 +5,11 @@ import com.jsp.ets.rating.RatingRequest;
 
 public class RatingMapper {
 	
-	public Rating mapToRatingEntity(RatingRequest ratingRequest, Rating rating) {
-		rating.setRating(ratingRequest.getRating());
-		rating.setSubject(ratingRequest.getSubject());
-		rating.setFeedback(ratingRequest.getFeedback());
-		
-	}
+//	public Rating mapToRatingEntity(RatingRequest ratingRequest, Rating rating) {
+//		rating.setRating(ratingRequest.getRating());
+//		rating.setSubject(ratingRequest.getSubject());
+//		rating.setFeedback(ratingRequest.getFeedback());
+//		
+//	}
 
 }
--- src/main/java/com/jsp/ets/user/Stack.java
+++ src/main/java/com/jsp/ets/user/Stack.java
@@ -1,9 +1,22 @@
 package com.jsp.ets.user;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum Stack {
-	
-	JAVA_FULL_STACK,
-	MERN_STACK,
-	PYTHON_FULL_STACK;
+
+	JAVA_FULL_STACK(EnumSet.of(Subject.CORE_JAVA, Subject.SQL, Subject.HIBERNATE, Subject.SPRING, Subject.SPRING_BOOT,Subject.HTML, Subject.CSS, Subject.JAVASCRIPT)),
+	MERN_STACK(EnumSet.of(Subject.HTML, Subject.CSS, Subject.JAVASCRIPT, Subject.REACTJS)),
+	PYTHON_FULL_STACK(EnumSet.of(Subject.PYTHON,Subject.SQL, Subject.HTML, Subject.CSS,Subject.JAVASCRIPT,Subject.DJANGO));
+
+	private final Set<Subject> subjects;
+
+	Stack(Set<Subject> subjects) {
+		this.subjects = subjects;
+	}
+
+	public Set<Subject> getSubjects() {
+		return subjects;
+	}
 
 }
