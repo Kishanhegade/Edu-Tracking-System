ba959bbb8ffd67647aca589fba0a33733e0e503a
KishanHegade <kishanhegade@gmail.com>
2024-08-30 15:17:03 +0530

    modified UserService code to have Java 8 Features

--- src/main/java/com/jsp/ets/user/Trainer.java
+++ src/main/java/com/jsp/ets/user/Trainer.java
@@ -5,7 +5,6 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/jsp/ets/user/UserService.java
+++ src/main/java/com/jsp/ets/user/UserService.java
@@ -22,71 +22,65 @@ public class UserService {
 	private UserRepository userRepo;
 	private UserMapper userMapper;
 	private RatingRepository ratingRepo;
-	
-	
-	public UserResponse saveUser(RegistrationRequest registrationRequest, UserRole role) {
-		User user = null;
-		switch (role) {
-		case ADMIN -> user = new Admin();
-		case HR -> user = new HR();
-		case STUDENT -> user = new Student();
-		case TRAINER -> user = new Trainer();
-		default -> throw new IllegalArgumentException("Unexpected value: " + role);
-		}
 
-		if(user != null) {
-			user = userMapper.mapToUserEntity(registrationRequest, user);
-			user.setRole(role);
-			user = userRepo.save(user);
-		}
+
+	public UserResponse saveUser(RegistrationRequest registrationRequest, UserRole role) {
+		User user = switch (role) {
+		case ADMIN -> new Admin();
+		case HR -> new HR();
+		case STUDENT -> new Student();
+		case TRAINER -> new Trainer();
+		default -> throw new IllegalArgumentException("Unexpected value: " + role);
+		};
+
+		user = userMapper.mapToUserEntity(registrationRequest, user);
+		user.setRole(role);
+		user = userRepo.save(user);
 
 		return userMapper.mapToUserResponse(user);
 	}
 
 	public UserResponse updateUser(UserRequest userRequest, String userId, UserRole role) {
-		Optional<User> optional = userRepo.findById(userId);
-		if(optional.isPresent()) {
-			switch (role) {
-			case STUDENT -> {
-				Student student = (Student)optional.get();
-				student = userMapper.mapToStudentEntity((StudentRequest)userRequest, student);
-				student = userRepo.save(student);
-				return userMapper.mapToStudentResponse(student);
-			}
-			case TRAINER -> {
-				Trainer trainer = (Trainer) optional.get();
-				trainer = userMapper.mapToTrainerEntity((TrainerRequest)userRequest, trainer);
-				trainer = userRepo.save(trainer);
-				return userMapper.mapToTrainerResponse(trainer);
-			}
-			default -> throw new IllegalArgumentException("Unexpected value: " + role);
-			}
-		}else {
-			throw new UserNotFoundByIdException("Failed to update User");
-		}
+		return userRepo.findById(userId)
+				.map(user -> {
+					switch (role) {
+					case STUDENT -> {
+						Student student = userMapper.mapToStudentEntity((StudentRequest) userRequest, (Student) user);
+						return userMapper.mapToStudentResponse(userRepo.save(student));
+					}
+					case TRAINER -> {
+						Trainer trainer = userMapper.mapToTrainerEntity((TrainerRequest) userRequest, (Trainer) user);
+						return userMapper.mapToTrainerResponse(userRepo.save(trainer));
+					}
+					default -> throw new IllegalArgumentException("Unexpected value: " + role);
+					}
+				})
+				.orElseThrow(() -> new UserNotFoundByIdException("Failed to update User"));
+
 	}
 
 	public StudentResponse updateStudentStack(String userId, String stack) {
-	    Student student = (Student) userRepo.findById(userId)
-	            .orElseThrow(() -> new UserNotFoundByIdException("Failed to update Stack"));
+		Student student = (Student) userRepo.findById(userId)
+				.orElseThrow(() -> new UserNotFoundByIdException("Failed to update Stack"));
 
-	    Stack techStack = Arrays.stream(Stack.values())
-	            .filter(s -> s.name().equalsIgnoreCase(stack))
-	            .findFirst()
-	            .orElseThrow(() -> new IllegalStackTypeException("Stack not found"));
-	    
-	    techStack.getSubjects().forEach(subject -> {
-	        Rating rating = new Rating();
-	        rating.setStudent(student);
-	        rating.setSubject(subject);
-	        ratingRepo.save(rating);
-	    });
-	    student.setStack(techStack);
-	    userRepo.save(student);
-	    return userMapper.mapToStudentResponse(student);
+		Stack techStack = Arrays.stream(Stack.values())
+				.filter(s -> s.name().equalsIgnoreCase(stack))
+				.findFirst()
+				.orElseThrow(() -> new IllegalStackTypeException("Stack not found"));
+
+		techStack.getSubjects().forEach(subject -> {
+			Rating rating = new Rating();
+			rating.setStudent(student);
+			rating.setSubject(subject);
+			ratingRepo.save(rating);
+		});
+
+		student.setStack(techStack);
+		userRepo.save(student);
+		return userMapper.mapToStudentResponse(student);
 	}
 
-	
+
 
 
 }
