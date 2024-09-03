60cb82c9d4008870bc186a4b0d1621580dfc9029
KishanHegade <kishanhegade@gmail.com>
2024-09-03 19:51:03 +0530

    Modified the class type in the classes attribute for the class annotated with @SpringBootTest

--- src/test/java/com/jsp/edu/Edu/Tracking/System/EduTrackingSystemApplicationTests.java
+++ src/test/java/com/jsp/edu/Edu/Tracking/System/EduTrackingSystemApplicationTests.java
@@ -1,14 +1,15 @@
 package com.jsp.edu.Edu.Tracking.System;
 
+import com.jsp.ets.EduTrackingSystemApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {EduTrackingSystemApplicationTests.class})
+@SpringBootTest(classes = {EduTrackingSystemApplication.class})
 class EduTrackingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// Yet to implement testing methods
+		// Yet to implement test cases
 	}
 
 }
