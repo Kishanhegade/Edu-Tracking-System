aac90a5c10cd9e3b6ab0b47081e7b49b3f44b671
KishanHegade <kishanhegade@gmail.com>
2024-09-03 22:05:43 +0530

    created application-test.yml file

--- src/test/java/com/jsp/edu/Edu/Tracking/System/EduTrackingSystemApplicationTests.java
+++ src/test/java/com/jsp/edu/Edu/Tracking/System/EduTrackingSystemApplicationTests.java
@@ -3,8 +3,10 @@ package com.jsp.edu.Edu.Tracking.System;
 import com.jsp.ets.EduTrackingSystemApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = {EduTrackingSystemApplication.class})
+@TestPropertySource(locations = "classpath:application-test.yml")
 class EduTrackingSystemApplicationTests {
 
 	@Test
