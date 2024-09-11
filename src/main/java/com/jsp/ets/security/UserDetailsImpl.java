e1ec1270592752c6c70dbfbac91e4dc98b6080f7
KishanHegade <kishanhegade@gmail.com>
2024-09-11 11:48:35 +0530

    Created UserDetailsImpl class which implements UserDetails interface

---
+++ src/main/java/com/jsp/ets/security/UserDetailsImpl.java
@@ -0,0 +1,24 @@
+package com.jsp.ets.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
