bcb90bd6caf821c4d042a57cd02c5298d5e669f2
KishanHegade <kishanhegade@gmail.com>
2024-09-17 13:23:08 +0530

    In JwtService implemented doFilterInternal method

--- src/main/java/com/jsp/ets/security/JwtFilter.java
+++ src/main/java/com/jsp/ets/security/JwtFilter.java
@@ -1,14 +1,21 @@
 package com.jsp.ets.security;
 
+import com.jsp.ets.user.UserRole;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
+
 @AllArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtService  jwtService;
@@ -22,7 +29,18 @@ public class JwtFilter extends OncePerRequestFilter {
                 Claims claims = jwtService.parseJwt(token);
                 String email = claims.get("email", String.class);
                 String role = claims.get("role", String.class);
+                if(role!=null && email!=null) {
+                    UserRole userRole = UserRole.valueOf(role);
+                    List<SimpleGrantedAuthority> simpleGrantedAuthorities = userRole.getPrivileges()
+                            .stream()
+                            .map(privilege-> new SimpleGrantedAuthority(privilege.name()))
+                            .toList();
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, userRole, simpleGrantedAuthorities);
+                    authenticationToken.setDetails(new WebAuthenticationDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
             }
+            filterChain.doFilter(request, response);
         }
 
     }
--- src/main/java/com/jsp/ets/security/SecurityConfig.java
+++ src/main/java/com/jsp/ets/security/SecurityConfig.java
@@ -1,29 +1,35 @@
 package com.jsp.ets.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    JwtService jwtService;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf->csrf.disable())
-                .authorizeHttpRequests(auth->auth.requestMatchers("/register/**","/users/verify","/login")
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/register/**", "/users/verify", "/login")
                         .permitAll()
                         .anyRequest().authenticated())
-                        .sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
                 .build();
-
     }
 
     @Bean
